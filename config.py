95db9cf82df04a8868f53b1ca7581d2ab003a2f4
Régis Behmo <regis@behmo.com>
2017-06-20 14:32:26 +0200

    Version config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+SECRET_KEY = ";G(cVsHnI0XQCwLx/_dp=.:~"
+FB_APP_ID = 1967148823570310
+
+# LOGIN
+SECURITY_PASSWORD_HASH = 'sha512_crypt'
+SECURITY_PASSWORD_SALT = 'fhasdgihwntlgy8f'
+SECURITY_POST_LOGIN_VIEW = '/dashboard'
+
+WTF_CSRF_ENABLED = True
+
+# Admin login
+ADMIN_EMAIL = 'matt@nobien.net'
+ADMIN_PW = 'supersuper'
+
+# Database settings
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+MAX_WORDS = 49
+
+BASE_URL = 'localhost:5000'
