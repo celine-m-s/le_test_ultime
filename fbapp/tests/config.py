7981065cd08e44687e9434ab8fe96054b6d62ba8
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-06-21 17:25:33 +0200

    trying to make tests

--- fbapp/tests/config.py
+++ fbapp/tests/config.py
@@ -5,7 +5,9 @@ import os
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
 SECRET_KEY = ";G(cVsHnI0XQCwLx/_dp=.:~"
 FB_APP_ID = 1967148823570310
-
+TESTING = True
+LIVESERVER_PORT = 8943
+LIVESERVER_TIMEOUT = 10
 # LOGIN
 SECURITY_PASSWORD_HASH = 'sha512_crypt'
 SECURITY_PASSWORD_SALT = 'fhasdgihwntlgy8f'
@@ -20,10 +22,9 @@ ADMIN_PW = 'supersuper'
 # Database settings
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI_TEST = 'sqlite:///' + os.path.join(basedir, 'app_test.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app_test.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 MAX_WORDS = 49
 
-BASE_URL = 'localhost:5000'
+BASE_URL = 'localhost:8943'
---
+++ fbapp/tests/test_user_takes_the_test.py
@@ -0,0 +1,50 @@
+import pytest
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from flask import Flask
+
+# class TestSelenium:
+#
+#     def test_it_works(self):
+#         driver = webdriver.Firefox()
+#         driver.get("http://www.python.org")
+#         assert "Python" in driver.title
+#         elem = driver.find_element_by_name("q")
+#         elem.clear()
+#         elem.send_keys("pycon")
+#         elem.send_keys(Keys.RETURN)
+#         assert "No results found." not in driver.page_source
+#         driver.close()
+
+class TestUserTakesTheTest(LiveServerTestCase):
+
+    def create_app(self):
+        # fbapp = Flask(__name__)
+        #
+        # # Config options - Make sure you created a 'config.py' file.
+        fbapp = Flask(__name__)
+
+        fbapp.config.from_object('config')
+        return app
+
+    def setUp(self):
+        """Setup the test driver and create test users"""
+        self.driver = webdriver.Firefox()
+        self.driver.get(self.get_server_url())
+    #
+    #     db.session.commit()
+    #     db.drop_all()
+    #     db.create_all()
+    #
+    # def tearDown(self):
+    #     self.driver.quit()
+
+    def test_server_is_up_and_running(self):
+        response = self.driver.get(self.get_server_url())
+        self.assertEqual(response.code, 200)
+    #
+    # def test_user_navigation(self):
+    #     DRIVER.get('/')
+    #     assert "Le test ultime !" in DRIVER.title
